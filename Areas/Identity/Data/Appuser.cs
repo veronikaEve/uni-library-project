30edaed5e088a04e7fa859c5340e44d2d11caaa8
FezaanSKY <60107735+FezaanSKY@users.noreply.github.com>
2020-11-17 10:18:52 +0000

    First Commit

---
+++ Areas/Identity/Data/Appuser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CO550LIbrary.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the Appuser class
+    public class Appuser : IdentityUser
+    {
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CO550LIbrary.Areas.Identity.Data;
+using CO550LIbrary.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CO550LIbrary.Areas.Identity.IdentityHostingStartup))]
+namespace CO550LIbrary.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CO550LIbraryDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CO550LIbraryDBContextConnection")));
+
+                services.AddDefaultIdentity<Appuser>(options => options.SignIn.RequireConfirmedAccount = false)
+
+                    .AddEntityFrameworkStores<CO550LIbraryDBContext>();
+            });
+        }
+    }
+}
